c6e009a2a2e9bc3266af5ba91936e4ead5988b41
kumjShid4 <minhanpro123@gmail.com>
2018-04-15 03:19:21 +0700

    add views to handle request

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import URLForm
+from .models import URL
 # Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        print(request.method)
+        form = URLForm(request.POST)
+        print(form.errors)
+        if form.is_valid():
+            url = form.save(commit=False)
+            url.save()
+            print(url)
+            return redirect('blog:index')
+    else:
+        form = URLForm()
+    urls = URL.objects.all()
+    return render(request, 'shortener/index.html', {'urls': urls})
+
+def shortener(request):
+    if request.method == "POST":
+        form = URLForm(request.POST)
+        if form.is_valid():
+            url = form.save(commit=False)
+            url.save()
+            return redirect('blog:index')
