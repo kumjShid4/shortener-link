6fd02a17f171ab9cb8236cdf8daf419cd313b304
kumjShid4 <minhanpro123@gmail.com>
2018-04-16 02:13:50 +0700

    modified url

--- shortener/models.py
+++ shortener/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from .utils import code_generator, create_shortcode
+from django_hosts.resolvers import reverse
 # Create your models here.
 
-class URLManager(models.Manager):
-    def all(self, *args, **kwargs):
-        qs_manager = super().all(*args, **kwargs)
-        return qs_manager.filter(active=True)
 
+class URLManager(models.Manager):
     def refresh_codes(self, *args, **kwargs):
         query_set = URL.objects.all()
         new_codes = 0
@@ -17,15 +15,14 @@ class URLManager(models.Manager):
             new_codes += 1
         return "New codes made: {i}".format(i=new_codes)
 
+
 class URL(models.Model):
     url = models.CharField(max_length=220)
     short_code = models.CharField(
         max_length=15, unique=True, null=False, blank=True)
-    
+
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
-    
     objects = URLManager()
 
     def __str__(self):
@@ -35,3 +32,9 @@ class URL(models.Model):
         if self.short_code == "" or self.short_code is None:
             self.short_code = create_shortcode(self)
         super(URL, self).save(*args, **kwargs)
+
+    def get_short_url(self):
+        url_path = reverse('redirect_url', kwargs={
+                           'shortcode': self.short_code}, host='www', scheme='http')
+        return url_path
+        # return "http://127.0.0.1:8000/{shortcode}".format(shortcode=self.short_code)
---
+++ shortener/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'shortener'
+
+urlpatterns = [
+    # url(r'^$', views.index, name='index'),
+    # url(r'^(?P<shortcode>[\w-]+)/$', views.redirect_url, name='redirect_url'),
+]
