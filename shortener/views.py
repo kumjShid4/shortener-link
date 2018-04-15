a34fb43df423488f41c51080c685a5ffc149f443
kumjShid4 <minhanpro123@gmail.com>
2018-04-16 02:14:42 +0700

    add count clink on link into view

--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .forms import URLForm
 from .models import URL
+from analytics.models import ClickCount
+
 # Create your views here.
 
 
@@ -10,18 +13,15 @@ def index(request):
         if form.is_valid():
             url = form.save(commit=False)
             url.save()
+            ClickCount.objects.create_event(url)
             return redirect('shortener:index')
     else:
         form = URLForm()
     urls = URL.objects.all()
-    form.fields['url'].widget.attrs = {"class": "form-control", 'placeholder':'Your original url here.'}
     return render(request, 'shortener/index.html', {'urls': urls, 'form': form})
 
 
-def shortener(request):
-    if request.method == "POST":
-        form = URLForm(request.POST)
-        if form.is_valid():
-            url = form.save(commit=False)
-            url.save()
-            return redirect('blog:index')
+def redirect_url(request, shortcode):
+    url = get_object_or_404(URL, short_code=shortcode)
+    ClickCount.objects.create_event(url)
+    return redirect(url.url)
