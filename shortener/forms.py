7be98c7cd3c36713ca67cf1d3d4e7daeee5c1c3a
kumjShid4 <minhanpro123@gmail.com>
2018-04-16 02:09:48 +0700

    add more styles for form

--- shortener/forms.py
+++ shortener/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from .models import URL
 
 class URLForm(forms.ModelForm):
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
     class Meta:
         model = URL
         fields = ('url',)
+        widgets = {
+            'url': forms.URLInput(attrs={'type':'url', 'class':'form-control', 'placeholder':'Your original url here'})
+        }
