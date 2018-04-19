671c94c079e04dc20c9c709337c990cbdf18812c
kumjShid4 <minhanpro123@gmail.com>
2018-04-19 16:53:46 +0700

    set DEBUG to False

--- kumishida/settings.py
+++ kumishida/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i^^h+y-(_$@l(+m21!lt(qi$x_=!za9!o*7n0wob*3lmmfqi!+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rutgonlink.herokuapp.com', '127.0.0.1']
 
