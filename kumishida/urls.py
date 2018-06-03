cec46ff8fff2de6d1cfd7e4a1a2a95170a0bc8c1
kumjShid4 <minhanpro123@gmail.com>
2018-06-04 00:07:40 +0700

    update urls

--- kumishida/urls.py
+++ kumishida/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from shortener.views import redirect_url, index, error404, error500, getClickCount
+from shortener.views import redirect_url, index, error404, error500
 from django.conf.urls import handler404, handler500
 
 urlpatterns = [
