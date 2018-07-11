49bcd7fa47f57b5b1645946c213624858c1a9997
kumjShid4 <minhanpro123@gmail.com>
2018-07-11 17:37:18 +0700

    update form

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from .models import URL
 
+
 class URLForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
     class Meta:
         model = URL
         fields = ('url',)
         widgets = {
-            'url': forms.URLInput(attrs={'type':'text', 'class':'form-control', 'placeholder':'Your original url here'})
+            'url': forms.URLInput(attrs={'type': 'text', 'class': 'form-control', 'placeholder': 'Your original url here', 'autocomplete': 'off', 'autofocus': ''})
         }
