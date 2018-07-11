8f0e4e3db92fbe14483dd411ad39c09e2b18c9b3
kumjShid4 <minhanpro123@gmail.com>
2018-07-11 17:38:38 +0700

    add url for changing url status

--- shortener/urls.py
+++ shortener/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'shortener'
 
 urlpatterns = [
-    url(r'^(?P<shortcode>[\w-]+)/$', views.getClickCount, name='getClickCount'),
+    url(r'^(?P<shortcode>[\w-]+)$', views.get_click_count, name='get_click_count'),
+    url(r'^change/(?P<shortcode>[\w-]+)/$', views.change_status, name='change_status'),
 ]
